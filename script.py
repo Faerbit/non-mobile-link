8c7de2f630d68b810be0d33a963e10e0659ca9a5
Faerbit <faerbit@server.fake>
2013-11-18 20:01:38 +0100

    Works for now. Further testing needed.

--- script.py
+++ script.py
@@ -2,8 +2,10 @@
 
 import praw
 import re
+import os
 
 def reply(comment, text):
+    text += "\n\n Got any problems with this bot? Message /u/faerbit"
     print("Replying:")
     print(text)
     comment.reply(text)
@@ -14,13 +16,14 @@ r = praw.Reddit(user_agent=user_agent)
 already_done = set()
 find_expression = re.compile("https?://[a-zA-Z]*\.m\.wikipedia\.org/wiki/[a-zA-Z0-9_#%]*")
 comments = r.get_subreddit('test').get_comments(limit=500)
-r.login("non-mobile-linkbot", "")
+r.login("non-mobile-linkbot", os.environ["NON_MOBILE_LINKBOT_PASSWORD"])
 for comment in comments:
     if comment.id not in already_done:
         text =""
-        links = re.findall(find_expression, comment.body)
-        for i in links:
+        links = []
+        for i in re.findall(find_expression, comment.body):
             i = re.sub("m\.", "", i)
+            links.append(i)
         if len(links) == 1:
             text = "Non-mobile link: "
             text += links[0]
@@ -28,8 +31,6 @@ for comment in comments:
         elif len(links) > 1:
             text = "Non-mobile links:"
             for i in links:
-                text += links[i]
-                text += "\n"
+                text += i
+                text += "\n\n"
             reply(comment, text)
-
-
