facd3a74b39f5c90020f56dc928c47fb1b58efd6
Faerbit <faerbit@server.fake>
2013-12-02 17:12:50 +0100

    Finished regex tests

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class TestRegex(unittest.TestCase):
     non_link_word = "Goat"
     test_links=[
         ("https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte",
-        "http://de.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte",
+        "https://de.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte",
         "Wikipedia regex failed.")
         ]
     used_regex = set()
@@ -25,44 +25,66 @@ class TestRegex(unittest.TestCase):
     def test_detect_mobile(self):
         for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
             test_string=mobile_link
-            result=TestRegex.helper_test_regex(test_string)
-            self.assertEqual(result, [mobile_link], msg)
+            self.assertEqual(TestRegex.helper_test_regex(test_string), [mobile_link], msg)
 
-    #def test_not_detect_not_mobile(self):
-    #    test_string ="https://de.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte"
-    #    links =[]
-    #    for i in re.findall(bot.regex.wikipedia_find, test_string):
-    #        links.append(i)
-    #    self.assertEqual([], links)
+    def test_not_detect_not_mobile(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string=non_mobile_link
+            self.assertEqual(TestRegex.helper_test_regex(test_string), [], msg)
 
-    #def test_only_detect_mobile(self):
-    #    test_string =("https://de.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte "
-    #        "https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte")
-    #    links =[]
-    #    for i in re.findall(bot.regex.wikipedia_find, test_string):
-    #        links.append(i)
-    #    self.assertEqual(["https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte"], links)
+    def test_only_detect_mobile(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string=non_mobile_link + " " + mobile_link
+            self.assertEqual(TestRegex.helper_test_regex(test_string), [mobile_link], msg)
 
-    #def test_detect_mobile_multiple_times(self):
-    #    test_string =("https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte "
-    #        "https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte")
-    #    links =[]
-    #    for i in re.findall(bot.regex.wikipedia_find, test_string):
-    #        links.append(i)
-    #    self.assertEqual(["https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte",
-    #            "https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte"], links)
+    def test_detect_mobile_multiple_times(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string=mobile_link + " " + non_mobile_link + " " + mobile_link
+            self.assertEqual(TestRegex.helper_test_regex(test_string), [mobile_link, mobile_link], msg)
 
-    #def test_detect_mobile_multiple_times_with_other_characters(self):
-    #    test_string =("https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte "
-    #        "aösldfj aösldfj öalsfdj öalskdjflöska asöldfljk "
-    #        "https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte "
-    #        "aösldfj aösldfj öalsfdj öalskdjflöska asöldfljk "
-    #        "https://de.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte")
-    #    links =[]
-    #    for i in re.findall(bot.regex.wikipedia_find, test_string):
-    #        links.append(i)
-    #    self.assertEqual(["https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte",
-    #            "https://de.m.wikipedia.org/wiki/Luftselbstverteidigungsstreitkr%C3%A4fte"], links)
+    def test_detect_link_encased_in_brackets(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string= "(" + mobile_link + ")" + "[" + mobile_link + "]" + "{" + mobile_link + "}" 
+            self.assertEqual(TestRegex.helper_test_regex(test_string), [mobile_link, mobile_link, mobile_link], msg)
+
+    def test_detect_mobile_multiple_times_with_other_text(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string= (TestRegex.non_link_word + " "
+                + mobile_link + " " + TestRegex.non_link_word + " " + non_mobile_link 
+                + " " + mobile_link + " " + TestRegex.non_link_word)
+            self.assertEqual(TestRegex.helper_test_regex(test_string), [mobile_link, mobile_link], msg)
+
+    def test_replace_mobile(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string=mobile_link
+            self.assertEqual(bot.replace_links(test_string), [non_mobile_link], msg)
+
+    def test_not_replace_not_mobile(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string=non_mobile_link
+            self.assertEqual(bot.replace_links(test_string), [], msg)
+
+    def test_only_replace_mobile(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string=non_mobile_link + " " + mobile_link
+            self.assertEqual(bot.replace_links(test_string), [non_mobile_link], msg)
+
+    def test_replace_mobile_multiple_times(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string=mobile_link + " " + non_mobile_link + " " + mobile_link
+            self.assertEqual(bot.replace_links(test_string), [non_mobile_link, non_mobile_link], msg)
+
+    def test_replace_link_encased_in_brackets(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string= "(" + mobile_link + ")" + "[" + mobile_link + "]" + "{" + mobile_link + "}" 
+            self.assertEqual(bot.replace_links(test_string), [non_mobile_link, non_mobile_link, non_mobile_link], msg)
+
+    def test_replace_mobile_multiple_times_with_other_text(self):
+        for (mobile_link, non_mobile_link, msg) in TestRegex.test_links:
+            test_string= (TestRegex.non_link_word + " "
+                + mobile_link + " " + TestRegex.non_link_word + " " + non_mobile_link 
+                + " " + mobile_link + " " + TestRegex.non_link_word)
+            self.assertEqual(bot.replace_links(test_string), [non_mobile_link, non_mobile_link], msg)
 
 if __name__ == "__main__":
     unittest.main()
