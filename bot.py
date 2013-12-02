29e505e7d0d2ea458c411d4315dee9a8fdebf2fc
Faerbit <faerbit@server.fake>
2013-12-03 00:23:03 +0100

    Refactored code to be more test friendly, also supports submissions now

--- bot.py
+++ bot.py
@@ -8,11 +8,14 @@ regex= [
     (re.compile("https?://[a-zA-Z]*\.m\.wikipedia\.org/wiki/[a-zA-Z0-9_#%]*"), re.compile("m\.")), #wikipedia
     ]
 
-def reply(comment, text):
-    text += ("\n\n ^Got ^any ^problems/suggestions ^with ^this ^bot? "
-        "^Message ^/u/faerbit ^or ^check ^out ^the ^[code](https://github.com/Faerbit/non-mobile-link)!")
-    comment.reply(text)
-    already_done.add(comment.id)
+def reply(reply_object, text):
+        text += ("\n\n ^Got ^any ^problems/suggestions ^with ^this ^bot? "
+            "^Message ^/u/faerbit ^or ^check ^out ^the ^[code](https://github.com/Faerbit/non-mobile-link)!")
+    if isinstance(reply_object, praw.object.Comment):
+        reply_object.reply(text)
+    if isinstance(reply_object, praw.object.Submission):
+        reply_object.add_comment(text)
+    return reply_object.id
 
 def replace_links(text):
     links = []
@@ -22,27 +25,37 @@ def replace_links(text):
             links.append(i)
     return links
 
-def main():
-    user_agent=("Non-mobile link 0.1 by /u/faerbit")
-    reddit = praw.Reddit(user_agent=user_agent)
-    already_done = set()
-    comments = reddit.get_subreddit('test').get_comments(limit=500)
-    reddit.login("non-mobile-linkbot", os.environ["NON_MOBILE_LINKBOT_PASSWORD"])
+def main(subreddit):
+    comments = reddit.get_comments(subreddit)
     for comment in comments:
         if comment.id not in already_done:
-            text =""
+            text = ""
             links = []
             links.append(replace_links(comment.body))
             if len(links) == 1:
                 text = "Non-mobile link: "
                 text += str(links[0])
-                reply(comment, text)
+                already_done.add(reply(comment, text))
             elif len(links) > 1:
                 text = "Non-mobile links:"
                 for i in links:
                     text += str(i)
                     text += "\n\n"
-                reply(comment, text)
+                already_done.add(reply(comment, text))
+    submissions = reddit.get_subreddit(subreddit)
+    for submission in submissions:
+        if submission.id not in already_done:
+            text = ""
+            links = []
+            links.append(replace_links(submission.url))
+            if len (links) == 1:
+                text = "Non-mobile link: "
+                text += str(links[0])
+                already_done.add(reply(text))
 
 if __name__ == "__main__":
-    main()
+    already_done = set()
+    user_agent=("Non-mobile link 0.1 by /u/faerbit")
+    reddit = praw.Reddit(user_agent=user_agent)
+    reddit.login("non-mobile-linkbot", os.environ["NON_MOBILE_LINKBOT_PASSWORD"])
+    main("test")
